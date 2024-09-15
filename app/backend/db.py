794e25d94adf7d0c5cbe83eda6995e449dc9e3b7
vkazakov127 <166021972+vkazakov127@users.noreply.github.com>
2024-09-15 20:10:40 +0300

    Add files via upload
    Module 17. Lesson "Миграции. Библиотека alembic."

---
+++ app/backend/db.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# /app/backend/db.py
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+engine = create_engine("sqlite:///taskmanager.db", echo=True)
+
+SessionLocal = sessionmaker(bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
+
---
+++ app/models/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8  -*-
+# /app/models/__init__.py
